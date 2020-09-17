58e8ae1da0dad1decd6c845d0c86e000383f262b
Yabir Garcia Benchakhtir <yabirgb@gmail.com>
2020-09-17 18:38:28 +0200

    Añade nuevo campo a la tabla de rutas

--- src/database/model.py
+++ src/database/model.py
@@ -14,6 +14,7 @@ db = PostgresqlExtDatabase('trabotories_db', user='tdb', password='tdb')
 class Ruta(Model):
 
     user = IntegerField()
+    message = IntegerField()
     date = DateTimeField()
     trajectory = JSONField()
     time = IntegerField( null=True )
@@ -32,4 +33,4 @@ def test_db():
 if __name__ == "__main__":
     load_dotenv()
     db.create_tables( [Ruta] )
-    test_db()
+    #test_db()

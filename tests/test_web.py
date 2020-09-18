13625f8601ebe02dd88503ba04df2e90b41fab75
Juanjo <jh16357@gmail.com>
2020-09-18 14:01:41 +0200

    Añado test para issue #13

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,9 +1,13 @@
+from datetime import datetime
 import pytest   
 from tassi.web_api import server
 from tassi.database.model import Ruta
 
+
 USER_ID1=2
 USER_ID2=4
+MESSAGE_ID1=6
+MESSAGE_ID2=34
 
 @pytest.fixture
 def client():
@@ -28,7 +32,7 @@ def test_get_routes(client):
 
     Ruta.create(
                     user= USER_ID1, 
-                    message = 6, 
+                    message = MESSAGE_ID1, 
                     date = time1, 
                     trajectory = coordinates1
                 )
@@ -37,20 +41,20 @@ def test_get_routes(client):
 
     Ruta.create(
                     user= USER_ID1, 
-                    message = 34, 
+                    message = MESSAGE_ID2, 
                     date = time2, 
                     trajectory = coordinates2
                 )
 
-    rutas=[{"user":USER_ID1,"message":6,"date": time1, "trajectory": coordinates1},
-    {"user":USER_ID1,"message":34,"date": time2, "trajectory": coordinates2}]
+    rutas=[{"user":USER_ID1,"message":MESSAGE_ID1,"date": time1, "trajectory": coordinates1},
+    {"user":USER_ID1,"message":MESSAGE_ID2,"date": time2, "trajectory": coordinates2}]
 
     rv = client.get(f'/api/v1/{USER_ID1}/my-routes')
     assert sorted(rutas.items()) == sorted(rv.data.items())                       
 
     #Undo changes in the database
-    Ruta.delete().where(Ruta.message == 6).execute()
-    Ruta.delete().where(Ruta.message == 34).execute()
+    Ruta.delete().where(Ruta.message == MESSAGE_ID1).execute()
+    Ruta.delete().where(Ruta.message == MESSAGE_ID2).execute()
 
 
 def test_emptyness(client):
@@ -58,3 +62,45 @@ def test_emptyness(client):
     rv = client.get(f'/api/v1/{USER_ID1}/my-routes')
 
     assert len(rv.data)==0
+
+def test_get_route_by_id(client):
+        
+    #Create some routes 
+    coordinates1=[{"longitude":2.2,"latitude":3.5},{"longitude":6.8,"latitude":7.4}]
+    time1=datetime.now()
+
+    Ruta.create(
+                    user= USER_ID1, 
+                    message = MESSAGE_ID1, 
+                    date = time1, 
+                    trajectory = coordinates1
+                )
+
+
+    ruta={"distance_total":46,"time":6,"date": time1,"speed_km":47,"speed_average":44 }
+
+    rv = client.get(f'/api/v1/{USER_ID1}/route/{MESSAGE_ID1}')
+    assert sorted(ruta.items()) == sorted(rv.data.items())
+
+    #TO-DO: checkout that the values are correct                       
+
+
+    #Undo changes in the database
+    Ruta.delete().where(Ruta.message == MESSAGE_ID1).execute()
+
+def test_delete_route_by_id(client):
+    
+    #Create a route
+    coordinates1=[{"longitude":2.2,"latitude":3.5},{"longitude":6.8,"latitude":7.4}]
+    time1=datetime.now()
+    
+    Ruta.create(
+                    user= USER_ID1, 
+                    message = MESSAGE_ID1, 
+                    date = time1, 
+                    trajectory = coordinates1
+                )
+
+    rv = client.delete(f'/api/v1/{USER_ID1}/route/{MESSAGE_ID1}')
+    assert len(rv.data) == 0 
+   
